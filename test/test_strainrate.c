5ba750137de5b4696e47102a11d9cf67683dc31c
Bray Moll <bkm82@nau.edu>
2025-02-06 00:12:14 -0700

    add strainrate test file

---
+++ test/test_strainrate.c
@@ -0,0 +1,22 @@
+#ifdef TEST
+#include "unity.h"
+#include "globals.h"
+#include "io.h"
+#include "macros.h"
+#include "memory.h"
+#include "mock_mesh.h"
+#include "structs.h"
+#include "mock_tracers.h"
+
+#include "strainrate.h"
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+
+#endif // TEST
